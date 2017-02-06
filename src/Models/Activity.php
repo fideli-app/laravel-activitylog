89f8fecaa73ec65d83c64c942308f38731b8ac1f
Huafu Gandon <huafu.gandon@gmail.com>
2017-02-06 23:28:25 +0700

    Update Activity.php

--- src/Models/Activity.php
+++ src/Models/Activity.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
-    use \Spatie\ActivityLog\Traits\ActivityModelTrait;
+    use \Spatie\ActivityLog\Traits\ActivityModelTrait
+    {
+        __construct as private _activityModelTraitConstructor;
+    }
+    
+    public function __construct(array $attributes = []) {
+        parent::__construct($attributes);
+        $this->_activityModelTraitConstructor($attributes, __CLASS__);
+    }
 }

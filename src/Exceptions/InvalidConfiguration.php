b6e94f9cb7d9b90364693fa8e9b99913981c5d4c
Huafu Gandon <huafu.gandon@gmail.com>
2017-02-07 14:11:14 +0700

    Update InvalidConfiguration.php

--- src/Exceptions/InvalidConfiguration.php
+++ src/Exceptions/InvalidConfiguration.php
@@ -3,12 +3,12 @@
 namespace Spatie\Activitylog\Exceptions;
 
 use Exception;
-use Spatie\Activitylog\Models\Activity;
+use Spatie\Activitylog\Interfaces\ActivityModelInterface;
 
 class InvalidConfiguration extends Exception
 {
     public static function modelIsNotValid(string $className)
     {
-        return new static("The given model class `$className` does not extend `".Activity::class.'`');
+        return new static("The given model class `$className` does not implement `".ActivityModelInterface::class.'`');
     }
 }

b4694d72dd8bcf7b3b05795d3b71834dc51c68c0
Huafu Gandon <huafu.gandon@gmail.com>
2017-02-07 14:26:44 +0700

    Update Activity.php

--- src/Models/Activity.php
+++ src/Models/Activity.php
@@ -3,8 +3,9 @@
 namespace Spatie\Activitylog\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Interfaces\ActivityModelInterface;
 
-class Activity extends Model
+class Activity extends Model implements ActivityModelInterface
 {
     use \Spatie\ActivityLog\Traits\ActivityModelTrait;
 }
